f3180bddff40a320adf03fba7ff86425ed4fe4b5
JulieAnd3 <julia@hiof.no>
2025-01-21 19:07:55 +0100

    Føler meg snart nå
    Gir jo så my mening at man bruker filter til å bare skrive ut en av informasjonen og man bare forandre på nøkkelen som er den som bestemmer hva som skal skrives ut! Mangler bare å fikse med CSS at den siden som er åpnet er hvit

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
-let resourcesPrint = ""
-let sourcesPrint = ""
 
-resources.map((resource, index) => resourcesPrint += `       
+let key = "HTML" //En variabel som forandrer seg utifra hvilken side brukeren er på
+
+//Variabler for de forskjellige knappene 
+const btnHTML = document.getElementById("htmlSite")
+const btnCSS = document.getElementById("cssSite")
+const btnJS = document.getElementById("javaSite")
+const btnReact = document.getElementById("reactSite")
+const btnCMS = document.getElementById("sanityCmsSite")
+
+//Event listner for knappene
+btnHTML.addEventListener("click", () =>{
+    key = "HTML"
+    resoursesFunc()
+})
+btnCSS.addEventListener("click", () =>{
+    key = "CSS"
+    resoursesFunc()
+})
+btnJS.addEventListener("click", () =>{
+    key = "JavaScript"
+    resoursesFunc()
+})
+btnReact.addEventListener("click", () =>{
+    key = "React"
+    resoursesFunc()
+})
+btnCMS.addEventListener("click", () =>{
+    key = "Sanity and headless CMS"
+    resoursesFunc()
+})
+
+function resoursesFunc(){
+    let resourcesPrint = ""
+    let sourcesPrint = "" //Det funker. Ikke rør. Gir ikke helt mening. Det går bra
+    
+    resources.filter((res) => res.category === key).map(resource => resourcesPrint += `       
         <article>
             <h2>${resource.category}</h2>
             <p>${resource.text}</p>
             <ul>
-            ${resource.sources.map(source => sourcesPrint = `
+            ${resource.sources.map(source => sourcesPrint = ` 
                 <li><a href="${source.url}">${source.title}</a></li>
-            `)}
+            `)} 
             </ul>
         </article>`) 
+        //Det funket faktisk og gir litt mening. Wow.
 
-    console.log(resourcesPrint)
+        //Printe ut resultatet 
+        document.getElementById("mainPrint").innerHTML = resourcesPrint
+}
 
-document.getElementById("mainPrint").innerHTML = resourcesPrint
+
+//Kan hele printe greien være en funskjon? Så når du trykker på knappen starter du funksjonen og den bare skriver ut den siden man skal ha?
+//Ikke helt vi forandrer hvilken nøkkel filteret ser etter også skriver den ut etter rikitg nøkkel
+
+
+resoursesFunc()
